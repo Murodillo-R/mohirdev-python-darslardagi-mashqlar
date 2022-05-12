f813b5686070adae6d2a0bce2eafbd7847dd4cc9
Murodillo-R <murodillorahmonov@mail.ru>
2022-05-12 08:35:56 +0500

    Create sonlar_3.py

---
+++ 6-dars Sonlar/sonlar_3.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 08:26:04 2022
+
+@author: Murodillo
+"""
+son1 = int(input("birinchi sonni kiriting: "))
+son2 = int(input("ikkinchi sonni kiriting: "))
+yigindi = son1+son2
+ayirma = son1-son2
+kopaytma = son1*son2
+bolinma = son1/son2
+print("Kiritilgan sonlarning yig‘indisi " + str(yigindi) + "ga teng \n" 
+      "Kiritilgan sonlarning ayirmasi " + str(ayirma) + "ga teng \n"
+      "Kiritilgan sonlarning ko‘paytmasi " + str(kopaytma) + "ga teng \n"
+      "Kiritilgan sonlarning bo‘linmasi " + str(bolinma) + "ga teng \n")
