195c000d3d473874b118ea427fa0d4d5a36b87b2
Murodillo-R <murodillorahmonov@mail.ru>
2022-05-12 08:35:43 +0500

    Create sonlar_1.py

---
+++ 6-dars Sonlar/sonlar_1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 08:17:46 2022
+
+@author: Murdillo
+"""
+#Foydalanuvchi kiritgan sonni kub va kvadratini chiqaruvchi dastur tuzish
+son = int(input("Sonnni kiriting"))
+kub = son**3
+kvadrat = son**2
+print("kiritilgan sonning kubi " + str(kub) + "va kiritilgan sonning kvadrati " + str(kvadrat))
