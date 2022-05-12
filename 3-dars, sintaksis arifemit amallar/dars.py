ca1f09c969fc6e333e6827061afee4d4a4975f75
Murodillo-R <murodillorahmonov@mail.ru>
2022-05-12 08:35:33 +0500

    Create dars.py

---
+++ 3-dars, sintaksis arifemit amallar/dars.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  9 23:56:22 2022
+
+@author: Murodillo
+"""
+# print('"Nexia", "Tiko", "Damas", ko‘rganlar qilar havas')
+
+# 5 ning 4-darajasi 
+# print(5**4) # javob:625
+
+#22 ni 4 ga bo‘lganda qoldiq nechaga teng
+#print(22%4) # javob: 2
+
+#Tomonlari 125 ga teng bo‘lgan kvadratning yuzi va premetirini toping
+#1)Yuzi
+#print(125*125) #Kvadratning yuzi: 15625
+#2 Premetri
+#print(4*125) #Premetri:500
+
+#Diametri 12 ga teng bo‘lgan doira yuzini toping (pi=3.14)
+#Formula S=D*pi
+#print(12*3.14) #javob:37.68
+
+#Katetlari 6 va 7 bo‘lgan to‘g‘ri burchakli uchburchakning gipatenuzasini toping
+#Formula a^2+b^2=c^2
+print((6**2+7**2)**(1/2))
