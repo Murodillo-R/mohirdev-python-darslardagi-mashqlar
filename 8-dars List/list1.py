1555423d6659388ee304b76624a33e50d0034d83
Murodillo-R <murodillorahmonov@mail.ru>
2022-05-12 08:36:04 +0500

    Create list1.py

---
+++ 8-dars List/list1.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 11 07:00:53 2022
+
+@author: Murodillo
+"""
+davlatlar = ['O‘zbekiston', 'Rassiya', 'Amerika', 'Xitoy']
+#print(davlatlar)
+uzunligi = len(davlatlar)
+#print(uzunligi)
+#print(sorted(davlatlar))
+#print(sorted(davlatlar, reverse = True))
+#print(davlatlar)
+#davlatlar.reverse()
+#print(davlatlar)
+#davlatlar.sort()
+#print(davlatlar)
+#davlatlar.sort(reverse = True)
+#print(davlatlar)
+juft_sonlar = list(range(120,1200,2))
+#print(juft_sonlar)
+#yigindi = sum(juft_sonlar) 
+#print(yigindi)
+#kichik = min(juft_sonlar)
+#katta = max(juft_sonlar)
+#print(katta-kichik)
+#print(juft_sonlar[0:5])
+#son = int(len(juft_sonlar)//2)
+#juft_sonlar.reverse()
+#son = int(len(juft_sonlar)//2)
+#print(juft_sonlar[0:20])
+taomlar = ['osh', 'manti', 'sho‘rva', 'mastva', 'shashlik']
+#print(taomlar)
+#nonushta = taomlar
+#print(nonushta)
+#nonushta[0:1]
+#print(nonushta)
+#nonushta.append('salat')
+#nonushta.append('masla')
+#print(nonushta)
+#print(taomlar)
+ozgarmas = ('osh', 'matni')
+print(ozgarmas)
+ozgarmas[0] = 'mastava'
+print(ozgarmas) 
