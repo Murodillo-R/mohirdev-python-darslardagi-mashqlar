55ea33926415d6446978362c3a98ef14f448d9a8
Murodillo-R <murodillorahmonov@mail.ru>
2022-05-12 08:35:23 +0500

    Create if_else1.py

---
+++ 10-dars if else/if_else1.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 11 21:18:22 2022
+
+@author: Murodillo
+"""
+#cars = ['toyota', 'mazda', 'hundai', 'gm', 'kia']
+#for car in cars:
+    #if car != 'gm':
+        #print(car.title())
+        
+    #else:
+     #   print(car.upper())        
+#login = input("loginni kiriting ") 
+#if login.lower() == 'admin':
+#    print("Xush kelibsiz, Admin. Foyalanuvchilar ro‘yxatini ko‘rasizmi?")
+#else:
+#    print(f"Xush kelibsiz {login}")
+son1 = int(input('Sonni kiriting'))
+#son2 = input('Ikkinchi sonni kiriting')
+if son1 > 0:
+    print(son1**(1/2))
+else:
+    print('Musbat son kiriting')
+
+        
+    
