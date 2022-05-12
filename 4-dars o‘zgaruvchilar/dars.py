d24f67e44608b7ffa06d1ba624c43dc5c91bedec
Murodillo-R <murodillorahmonov@mail.ru>
2022-05-12 08:35:38 +0500

    Create dars.py

---
+++ 4-dars o‘zgaruvchilar/dars.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 00:14:49 2022
+
+@author: Murodillo
+"""
+
+#1-mashq
+#hello="Hello World!"
+#print(hello)
+
+#2-mashq
+#xabar="Bugun Toshkenda havo 23C tashkil etmoqada."
+#print(xabar)
+#xabar="Python dasturlash tilini imkoniyatlari ko‘p"
+#print(xabar)
+
+#3-mashq
+#class="Bu xato"
+#print(class)
+
+#4-mashq
+#radus=5
+#pi=3.14159
+#aylana_yuzi=pi*radus**2
+#print("Radusi", radus, "ga teng bo‘lgan aylana yuzi", aylana_yuzi)
