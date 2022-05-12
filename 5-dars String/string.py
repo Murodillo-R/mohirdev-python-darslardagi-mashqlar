5114850d1325f4b08a6f7a29c60e62e26c768aff
Murodillo-R <murodillorahmonov@mail.ru>
2022-05-12 08:35:52 +0500

    Create string.py

---
+++ 5-dars String/string.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 00:41:31 2022
+
+@author: Murodillo
+"""
+#kocha = 'Bog‘bon'
+#mahalla = 'Sog‘bon'
+#tuman = 'Bodomzor'
+#viloyat  = 'Samarqand'
+
+
+kocha = input('kochani kiriting')
+mahalla = input("Mahalla nomini kiriting")
+tuman = input("Tuman nomini kiriting")
+viloyat = input("Viloyat nomi kiriting")
+print(f"{viloyat} viloyati {tuman} tamani {mahalla} mahallasi {kocha} kochasi")
