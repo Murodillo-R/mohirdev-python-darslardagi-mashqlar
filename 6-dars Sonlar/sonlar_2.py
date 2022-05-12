cc9a49462363cc63f0a81e60a80d1cc872b76b84
Murodillo-R <murodillorahmonov@mail.ru>
2022-05-12 08:35:47 +0500

    Create sonlar_2.py

---
+++ 6-dars Sonlar/sonlar_2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 08:22:13 2022
+
+@author: Murodillo
+"""
+
+#Foydalanuvchi yoshini so‘rang
+yosh = int(input("Yoshingizni kiriting:" ))
+t_yili = 2022-yosh;
+print("Sizning tug‘ulgan yilingiz :" + str(t_yili)) 
