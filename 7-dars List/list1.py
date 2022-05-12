754fe49474f6b06d4a828435c334bfc714e86f01
Murodillo-R <murodillorahmonov@mail.ru>
2022-05-12 08:36:00 +0500

    Create list1.py

---
+++ 7-dars List/list1.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 21:25:59 2022
+
+@author: Murodillo
+"""
+#Do‘stlarning ismlari 
+
+#ismlar = ['Diyorbek', 'Ozodbek', 'Mavlon']
+#print(ismlar)
+#ismlar.append('Otabek')
+#print(ismlar)
+#ismlar.remove('Otabek')
+#print(ismlar)
+#print("Salom " + ismlar[0])
+#print(ismlar[2] + "nomeri bormi")
+
+#sonlar = [1200, 12.6, -10, 'ikki']
+#print(sonlar)
+#sonlar[0] = sonlar[0] + 200
+#print(sonlar)
+
+#2 ta  t_shaxslar va z_shaxslar ro‘yxatini yaratish
+#t_shaxslar = ['Xorazmiy', 'Beruniy', 'Abu Ali Ibn Sino']
+#z_shaxslar = ['Bill Geyts', 'Stiv Jobs', 'Jek Ma']
+#qiymat = t_shaxslar.pop(0)
+#qiymat2 = z_shaxslar.pop(0)
+#print(f"Men tarixiy shaxslardan {qiymat} bilan va zamonaviy shaxslarda {qiymat2} bilan suxbat qurishhni xohlardim")
+
+#frends nomli ro‘yxat
+#frends = []
+#frends.append('Malik')
+#frends.append('Dilyor')
+#frends.append('Kamol')
+#frends.remove('Malik')
+#frends.insert(0, 'Malik')
+#frends.insert(2, "Komil")
+#mehmon = []
+#mehmon.append(frends.pop(0))
+#print(mehmon)
+
