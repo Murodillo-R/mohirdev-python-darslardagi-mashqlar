b006410de4030d2bba1fe97a2bcaa2c10bb05cde
Murodillo-R <murodillorahmonov@mail.ru>
2022-05-13 21:51:59 +0500

    Delete 14-dars Lug‘at.py

--- 14-dars Lug‘at.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 12 22:08:36 2022
-
-@author: Murodillo
-"""
-otam = {'ismi': 'Xamidullo', 'tug_yili':'1968', 'viloyati':'Andijon'}
-print(f"Mening dadam {otam['ismi']} {otam['viloyati']} viloyatida {otam['tug_yili']} yili tug‘ulgan" )
