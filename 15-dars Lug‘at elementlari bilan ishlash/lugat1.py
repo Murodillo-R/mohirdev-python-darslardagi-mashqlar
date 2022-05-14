bccba1625cff05e130ed762b86940e7c1cff796f
Murodillo-R <murodillorahmonov@mail.ru>
2022-05-14 18:43:02 +0500

    Create lugat1.py

---
+++ 15-dars Lug‘at elementlari bilan ishlash/lugat1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 13 21:52:15 2022
+
+@author: Murodillo
+"""
+
+#Python izohli lug'atini yarating va lug'atga kamida 10 ta so'z qo'shing. 
+#Lug'atdagi har bir kalit va qiymatni for tsikli yordamida, 
+#alifbo ketma-ketligida chiroyli qilib konsolga chiqaring
+
+python_izohli_lugat = {
+    'int': 'Butun son',
+    'string':'Matnli qiymat',
+    'title()':'So‘zlarni bosh harfini katta qilish',
+    'upper()': 'Sozlarni barcha harflarni katta qilish',
+    'remove()': 'Royxatdagi ma\'lumotni ochirish',
+    
+    }
+for kalit, qiymat in python_izohli_lugat:
+    print(kalit)
