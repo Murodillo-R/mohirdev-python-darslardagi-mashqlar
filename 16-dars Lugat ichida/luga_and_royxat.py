57fbaab97b73c709382651aaa0a771577d5826b2
Murodillo-R <murodillorahmonov@mail.ru>
2022-05-14 18:15:46 +0500

    Create luga_and_royxat.py

---
+++ 16-dars Lugat ichida/luga_and_royxat.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 14 12:31:25 2022
+
+@author: Murodillo
+"""
+mashxur0 = {
+    'ismi':'zaynobiddinov Sirojiddin',
+    'tyili': '1958',
+    'kasbi': 'yadro tadqoqodchi'
+    }
+
+mashxur1 = {
+    'ismi':'bill geyts',
+    'tyili': '1956',
+    'kasbi': 'ixtrochi'
+    }
+
+mashxur2 = {
+    'ismi':'Xushnydbek ',
+    'tyili': '1986',
+    'kasbi': 'huquqshunos'
+    }
+mashxur_odamlar = [mashxur0, mashxur1, mashxur2 ]
+for mashxur_odam in mashxur_odamlar:
+
+        print(mashxur_odam)
